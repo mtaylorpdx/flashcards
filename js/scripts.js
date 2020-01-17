39fbf487a5d0b6d1eacbb07611889a609a37c73f
mtaylorpdx <taylor.matt@protonmail.com>
2020-01-16 20:42:56 -0800

    create restructuring from list to panels

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,72 @@
 $(document).ready(function() {
 
-  $(".js-h").click(function() {
-    $(".js-p").toggle();
+  $("#js-h").click(function() {
+    $("#js-p").toggle();
   });
-  $(".op-h").click(function() {
-    $(".op-p").toggle();
+  $("#js-p").click(function() {
+    $("#js-h").toggle();
   });
-  $(".va-h").click(function() {
-    $(".va-p").toggle();
-  });
-  $(".vc-h").click(function() {
-    $(".vc-p").toggle();
-  });
-  $(".fc-h").click(function() {
-    $(".fc-p").toggle();
-  });
-  $(".me-h").click(function() {
-    $(".me-p").toggle();
-  });
-  $(".ar-h").click(function() {
-    $(".ar-p").toggle();
-  });
-  $(".pa-h").click(function() {
-    $(".pa-p").toggle();
-  });
-  $(".re-h").click(function() {
-    $(".re-p").toggle();
-  });
-  $(".cm-h").click(function() {
-    $(".cm-p").toggle();
-  });
-  $(".st-h").click(function() {
-    $(".st-p").toggle();
-  });
-  $(".bo-h").click(function() {
-    $(".bo-p").toggle();
-  });
-  $(".un-h").click(function() {
-    $(".un-p").toggle();
-  });
-  $(".nan-h").click(function() {
-    $(".nan-p").toggle();
-  });
-  $(".es-h").click(function() {
-    $(".es-p").toggle();
-  });
-  $(".al-h").click(function() {
-    $(".al-p").toggle();
-  });
-  $(".co-h").click(function() {
-    $(".co-p").toggle();
-  });
-  $(".jq-h").click(function() {
-    $(".jq-p").toggle();
-  });
-  $(".at-h").click(function() {
-    $(".at-p").toggle();
-  });
+
+
+
 });
+
+
+
+
+
+
+  // $(".op-h").click(function() {
+  //   $(".op-p").toggle();
+  // });
+  // $(".va-h").click(function() {
+  //   $(".va-p").toggle();
+  // });
+  // $(".vc-h").click(function() {
+  //   $(".vc-p").toggle();
+  // });
+  // $(".fc-h").click(function() {
+  //   $(".fc-p").toggle();
+  // });
+  // $(".me-h").click(function() {
+  //   $(".me-p").toggle();
+  // });
+  // $(".ar-h").click(function() {
+  //   $(".ar-p").toggle();
+  // });
+  // $(".pa-h").click(function() {
+  //   $(".pa-p").toggle();
+  // });
+  // $(".re-h").click(function() {
+  //   $(".re-p").toggle();
+  // });
+  // $(".cm-h").click(function() {
+  //   $(".cm-p").toggle();
+  // });
+  // $(".st-h").click(function() {
+  //   $(".st-p").toggle();
+  // });
+  // $(".bo-h").click(function() {
+  //   $(".bo-p").toggle();
+  // });
+  // $(".un-h").click(function() {
+  //   $(".un-p").toggle();
+  // });
+  // $(".nan-h").click(function() {
+  //   $(".nan-p").toggle();
+  // });
+  // $(".es-h").click(function() {
+  //   $(".es-p").toggle();
+  // });
+  // $(".al-h").click(function() {
+  //   $(".al-p").toggle();
+  // });
+  // $(".co-h").click(function() {
+  //   $(".co-p").toggle();
+  // });
+  // $(".jq-h").click(function() {
+  //   $(".jq-p").toggle();
+  // });
+  // $(".at-h").click(function() {
+  //   $(".at-p").toggle();
+  // });
